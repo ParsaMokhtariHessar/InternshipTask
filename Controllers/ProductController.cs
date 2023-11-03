27fa8f8ef1af5413031a6d63c34ceb56d86b0bf5
Parsa <parsamokhtarihessar@gmail.com>
2023-11-03 19:03:40 +0330

    PUT and Dtos and Services!

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -10,6 +10,28 @@ namespace InternshipTask.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        
+        private static List<Product> Products = new List<Product>{
+            new Product(),
+            new Product { Id=1 ,Name = "Pouriya"}
+        };
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult<Product> GetProducts()
+        {
+            return Ok(Products);
+        }
+        [HttpGet]
+        [Route("GetSingle/{id}")]
+        public ActionResult<Product> GetSingleProduct(int id)
+        {
+            return Ok(Products.FirstOrDefault(c => c.Id == id ));
+        }
+        [HttpPost]
+        [Route("PostSingle")]
+        public ActionResult<Product> AddProduct(Product newProduct)
+        {
+            Products.Add(newProduct);
+            return Ok(Products);
+        }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,11 @@
 namespace InternshipTask;
 public class Product
 {
-    public int Id {get; set;}
-    public string? Name {get; set;}
-    public DateTime ProductDate {get; set;}
-    public string? ManufacturePhone {get; set;}
-    public string? ManufactureEmail {get; set;}
-    public bool IsAvailable {get; set;}
+    public int Id {get; set;} = 0;
+    public string? Name {get; set;} = "Parsa";
+    public DateTime ProductDate {get; set;} = DateTime.Now;
+    public string? ManufacturePhone {get; set;} = "09934463133";
+    public string? ManufactureEmail {get; set;} = "MJ@Parsa.com";
+    public bool IsAvailable {get; set;} = true;
     
 }

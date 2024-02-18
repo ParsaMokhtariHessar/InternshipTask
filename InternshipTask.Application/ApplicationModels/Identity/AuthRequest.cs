2264d527a95e5ff6d028ae852a12005fe72b86e4
Parsa <parsamokhtarihessar@gmail.com>
2024-02-18 05:10:26 +0330

    deleted again

--- InternshipTask.Application/ApplicationModels/Identity/AuthRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InternshipTask.Application.ApplicationModels.Identity
-{
-    public class AuthRequest
-    {
-        public string UserName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
--- InternshipTask.Application/ApplicationModels/Identity/AuthResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InternshipTask.Application.ApplicationModels.Identity
-{
-    public class AuthResponse
-    {
-        public Guid UserId { get; set; }
-        public string UserName { get; set; } = string.Empty;
-        public string Token { get; set; } = string.Empty;
-    }
-}
